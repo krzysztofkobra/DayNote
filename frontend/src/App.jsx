14fc74338a4d9b7bede63dad2c4a6d2c5f957e48
krzysztofkobra <foxy.scarry@gmail.com>
2025-06-23 20:58:01 +0200

    notes.jsx start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 import Calendar from './components/Calendar'
+import Notes from './components/Notes'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/notes" element={<Notes />} />
         <Route path="/" element={<Calendar />}/>
       </Routes>
     </Router>
